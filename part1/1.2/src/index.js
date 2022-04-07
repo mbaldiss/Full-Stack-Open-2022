25d67a21c1b5bfe949b790edbf2e2bfa78813616
Marco Baldissone <marcol2c4@gmail.com>
2022-04-07 09:41:03 -0300

    part1: 1.2 done

---
+++ part1/1.2/src/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+/* 
+I am use React v18
+ */
+
+import { createRoot } from 'react-dom/client';
+
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
+const App = () => {
+  return (
+    <div>
+      <Header course={course}/>
+      <Content />
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.total}
+    </p>
+  )
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+/* ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+); */
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
